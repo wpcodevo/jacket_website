9ef9eb32bf77c731d25dee841c2426e54375ebcd
Ziddah Edem <ziddah.edem@gmail.com>
2020-09-14 12:53:12 +0000

    :tada: initial commit

---
+++ js/index.js
@@ -0,0 +1,36 @@
+/*
+Navigation
+*/
+
+const navBar = document.querySelector(".navigation");
+
+window.addEventListener("scroll", () => {
+  const navHeight = navBar.getBoundingClientRect().height;
+  const scrollHeight = window.pageYOffset;
+
+  if (scrollHeight > navHeight) {
+    navBar.classList.add("fix__nav");
+  } else {
+    navBar.classList.remove("fix__nav");
+  }
+});
+
+const navOpen = document.querySelector(".nav__hamburger");
+const navClose = document.querySelector(".close__toggle");
+const menu = document.querySelector(".nav__menu");
+
+navOpen.addEventListener("click", () => {
+  const navLeft = menu.getBoundingClientRect().left;
+  if (navLeft < 0) {
+    menu.style.left = "0";
+  }
+});
+
+navClose.addEventListener("click", () => {
+  const navLeft = menu.getBoundingClientRect().left;
+  if (navLeft > 0) {
+    menu.style.left = "0";
+  } else {
+    menu.style.left = "-40rem";
+  }
+});
--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,9 @@
+const slider1 = document.getElementById("glide1");
 
+new Glide(slider1, {
+  type: "carousel",
+  startAt: 0,
+  perview: 1,
+  animationDuration: 800,
+  animationTimingFunc: "linear",
+}).mount();
