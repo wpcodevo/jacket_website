b42753375b5d121fb28a0047ec7738e95e8a941b
Ziddah Edem <ziddah.edem@gmail.com>
2020-09-14 18:09:48 +0000

    :tada initial commit:

--- js/index.js
+++ js/index.js
@@ -23,6 +23,7 @@ navOpen.addEventListener("click", () => {
   const navLeft = menu.getBoundingClientRect().left;
   if (navLeft < 0) {
     menu.style.left = "0";
+    document.body.classList.add("active");
   }
 });
 
@@ -32,5 +33,6 @@ navClose.addEventListener("click", () => {
     menu.style.left = "0";
   } else {
     menu.style.left = "-40rem";
+    document.body.classList.remove("active");
   }
 });
