0c5b4ea15d381d25e9aa56ea870d7fb0d6ab2191
Ziddah Edem <ziddah.edem@gmail.com>
2020-09-16 18:40:15 +0000

    Delete slider.js

--- js/slider.js
+++
@@ -1,9 +0,0 @@
-const slider1 = document.getElementById("glide1");
-
-new Glide(slider1, {
-  type: "carousel",
-  startAt: 0,
-  perview: 1,
-  animationDuration: 800,
-  animationTimingFunc: "linear",
-}).mount();
